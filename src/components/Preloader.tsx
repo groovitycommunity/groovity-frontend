adc5fe3940b1cded94c6dd968e6750ac8c7380dc
Raj Sachan <merajsachan@gmail.com>
2025-11-16 14:05:58 +0530

    Update Preloader.tsx

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -11,7 +11,7 @@ export default function Preloader() {
     const fallbackTimer = setTimeout(() => {
       console.log('Preloader fallback timeout triggered');
       handleTransition();
-    }, 8000);
+    }, 10000);
 
     return () => clearTimeout(fallbackTimer);
   }, [isLoading]);
