4e08330cc56c154dcd5cf9751ad9d69d62251a91
vishwesh singh <vishweshsingh52@gmail.com>
2026-02-15 04:08:15 +0530

    Fixed UI Issues

--- src/components/BackgroundCanvas.tsx
+++ src/components/BackgroundCanvas.tsx
@@ -52,7 +52,7 @@ export default function BackgroundCanvas() {
 
 
   return (
-    <div className="fixed left-0 right-0 z-0 overflow-hidden pointer-events-none"
+    <div className="fixed left-0 right-0 z-0 bottom-0 overflow-hidden pointer-events-none"
       style={{
     top: "5rem",
     height: "calc(var(--app-height) - 5rem)"
